09fd0119cf5b2b38bbfd49db08ea9828abd74ad5
Sanghyuk Jeong <shjeong920522@gmail.com>
2021-05-03 22:03:57 +0900

    implement React Router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from './Rating'
+import { Link } from 'react-router-dom';
+
 const numberWithCommas = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+};
 const Product = ({ product }) => {
   return (
-    <Card className="my-3 py-3 rounded">
-      <a href={`/product/${product._id}`}>
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text>
           <div className="my-3">
-            {product.rating}{product.rating>1? " stars" : " star"} from {product.numReviews} {product.numReviews>1? "people" : "person"}
+            <Rating value ={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
           </div>
         </Card.Text>
         <Card.Text as="h3">{numberWithCommas(product.price)}₩</Card.Text>
