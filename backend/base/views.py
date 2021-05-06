7577cf0796f0f8fee4bb47140170bdcef6d3c497
Sanghyuk Jeong <shjeong920522@gmail.com>
2021-05-06 20:07:46 +0900

    add backend authentication using customed jwt

--- backend/base/views.py
+++ backend/base/views.py
@@ -6,7 +6,23 @@ from rest_framework.response import Response
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 # Create your views here.
 @api_view(['GET'])
 def getRoute(request): 
