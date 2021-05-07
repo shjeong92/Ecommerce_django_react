9712cd41bc40b707b0201eaf01ee7904e9711d6b
Sanghyuk Jeong <shjeong920522@gmail.com>
2021-05-07 16:57:49 +0900

    Payment Screen & Save Payment Method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 function App() {
   return (
     <Router>
@@ -21,6 +22,7 @@ function App() {
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from '../actions/cartActions';
+
 const ShippingScreen = ({ history }) => {
 
   const cart = useSelector(state => state.cart)
@@ -15,12 +17,14 @@ const ShippingScreen = ({ history }) => {
   
   const submitHandler = (e) => {
       e.preventDefault()
+      console.log('asdf');
       dispatch(saveShippingAddress({address, city, postalCode, country}))
       history.push('/payment')
   };
 
   return (
     <FormContainer>
+    <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
